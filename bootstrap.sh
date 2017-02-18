6537724bba5d3b6b5afde109fa0773ee30c43f5c
swal4u <stephane.walter@me.com>
2017-02-18 11:23:39 +0100

    Mise à jour du bootstrap
    Suppression de certains outils
    Ajout de binIp pour accéder à distance à la machine mongo

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,10 +13,6 @@ sudo ntpdate 0.fr.pool.ntp.org
 sudo service ntpd start
 sudo timedatectl set-timezone Europe/Paris
 
-## Installation d'outils complémentaires
-#sudo yum groupinstall -q -y "Outils de développement"
-#sudo yum install -q -y wget
-
 ## Désactivation du firewall
 sudo systemctl disable firewalld
 sudo systemctl stop firewalld
@@ -36,8 +32,12 @@ sudo sed -i 's|4096|32000|' /etc/security/limits.d/20-nproc.conf
 sudo cp /vagrant/mongodb-org-3.2.repo /etc/yum.repos.d/
 sudo yum makecache
 sudo yum install -y mongodb-org
+## Permettre à des machines distantes d'accéder à la machine mongo
+sudo sed -i 's/bindIp: 127.0.0.1/#bindIp: 127.0.0.1/g' /etc/mongod.conf
 sudo service mongod start
 sudo chkconfig mongod on
+
+
 
 
 
